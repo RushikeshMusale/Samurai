17156747f5741abda45fbc18676e52a351f5162a
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-06-25 21:49:16 +0530

    Added Console Logger to log commands to console

--- SamuraiApp.Data/SamuraiDbContext.cs
+++ SamuraiApp.Data/SamuraiDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SumaraiApp.Domain;
 using System;
 using System.Collections.Generic;
@@ -13,20 +14,33 @@ namespace SamuraiApp.Data
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
 
-        public DbSet<Battle> Battles  { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+             {
+                 builder
+                  .AddFilter((category, level) =>
+                      category == DbLoggerCategory.Database.Command.Name
+                     && level == LogLevel.Information)
+                  .AddConsole(); // requires package Microsoft.Extensions.Logging.Console
+             });
+
 
         // One way to add connection string
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            optionsBuilder.UseSqlServer("Data Source=(localDB)\\MSSQLLocalDB; Initial Catalog =SamuraiAppData ");
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .UseSqlServer("Data Source=(localDB)\\MSSQLLocalDB; Initial Catalog =SamuraiAppData ");
             // not needed to call base method
             //base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<SamuraiBattle>().HasKey(sb => new { sb.SamuraiId, sb.BattleId});
+            modelBuilder.Entity<SamuraiBattle>().HasKey(sb => new { sb.SamuraiId, sb.BattleId });
             // since Horse is not added as DBSet but as navigation property inside Samurai,
             // EF will create a table as Horse, but to pluralize that we use fluent api here
             modelBuilder.Entity<Horse>().ToTable("Horses");

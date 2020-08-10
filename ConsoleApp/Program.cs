5333961e7c8ec905a3724d58e5a770f1fb3fcf0a
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-08-10 13:30:21 +0530

    Difference between Find & other linq queries like where

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using SamuraiApp.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
 using SumaraiApp.Domain;
 using System;
 using System.Linq;
@@ -14,6 +15,8 @@ namespace ConsoleApp
         {
             context.Database.EnsureCreated();
 
+            DifferenceBetweenFindAndWhere();
+
             InsertMultipleSamurais();
 
             //GetSamurais("Before Add:");
@@ -21,6 +24,22 @@ namespace ConsoleApp
             //GetSamurais("After Add:");
             Console.Write("Press any key...");
             Console.ReadKey();
+
+
+        }
+
+        private static void DifferenceBetweenFindAndWhere()
+        {
+            var samurai = context.Samurais.FirstOrDefault(s => s.Id == 1);
+            Console.WriteLine("Name of Samurai: "+ samurai.Name);
+
+            //samurai.Name = "Updated back";
+
+            var samuraiFound = context.Samurais.Find(1);
+            Console.WriteLine("Check the logs, it will not run any extra query, since we had searched it earlier");
+
+            var samuraiWhere = context.Samurais.Where(s => s.Id == 1).FirstOrDefault();
+            Console.WriteLine("Check the logs, this will run one more query");
         }
 
         private static void InsertMultipleSamurais()
@@ -32,6 +51,8 @@ namespace ConsoleApp
             // If the number of samurais are less than 4, then there will be separate command sent to sql server
             // otherwise single command will be executed for inserting
             context.Samurais.AddRange(samurai, samurai2,samurai3, samurai4);
+
+            //context.Samurais.Where(s=> EF.Functions.Like())
 
             context.SaveChanges();
         }
